16b0dce56bc828439b899177d1a4275a5c48553d
Miguel <ggmiguel1995@gmail.com>
2023-10-18 16:37:09 +0200

    variables inicializadas

---
+++ game.js
@@ -0,0 +1,16 @@
+function initialize(){
+    const rockBtn = document.getElementById("rock");
+    const paperBtn = document.getElementById("paper");
+    const scizorsBtn = document.getElementById("scissors");
+    const lizardBtn = document.getElementById("lizard");
+    const spockBtn = document.getElementById("spock");
+}
+
+window.addEventListener("load", ()=>{
+    initialize;
+    
+
+
+
+
+});
