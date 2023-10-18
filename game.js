d8fbcee9c3ff70a81a07ef6ac72ecdbdff145632
Miguel <ggmiguel1995@gmail.com>
2023-10-18 17:08:56 +0200

    add initialize

--- game.js
+++ game.js
@@ -1,16 +1,61 @@
-function initialize(){
-    const rockBtn = document.getElementById("rock");
-    const paperBtn = document.getElementById("paper");
-    const scizorsBtn = document.getElementById("scissors");
-    const lizardBtn = document.getElementById("lizard");
-    const spockBtn = document.getElementById("spock");
-}
+  const ROCK = 0;
+  const PAPER = 1;
+  const SZISSORS = 2;
+  const LIZARD = 3;
+  const SPOCK = 4;
 
-window.addEventListener("load", ()=>{
-    initialize;
+  const WIN = 0;
+  const LOST = 1;
+  const TIE = 2;
+
+  let rockBtn;  
+  let paperBtn;
+  let scizorsBtn;
+  let lizardBtn;
+  let spockBtn;
+    
     
 
+    
+function initialize(){
+    rockBtn = document.getElementById("rock");
+    paperBtn = document.getElementById("paper");
+    scizorsBtn = document.getElementById("scissors");
+    lizardBtn = document.getElementById("lizard");
+    spockBtn = document.getElementById("spock");
+   
+
+}
+
+function setListeners() {
+    rockBtn.addEventListener("click", ()=> {
+        play(ROCK);
+    });
+    paperBtn.addEventListener("click", ()=>{
+        play(PAPER);
+    });
+    scizorsBtn.addEventListener("click", ()=>{
+        play(SZISSORS);
+    });
+    lizardBtn.addEventListener("click", ()=>{
+        play(LIZARD);
+    });
+    spockBtn.addEventListener("click", ()=>{
+        play(SPOCK);
+    });
+}
+function play(userOption){
+    const machineOption = Math.floor(Math.random() * 5);
+    console.log(machineOption);
+
+    if(userOption === machineOption) {
+        return TIE;
+    }
+}
 
 
+window.addEventListener("load", ()=>{
+    initialize();
+    setListeners();
 
 });
